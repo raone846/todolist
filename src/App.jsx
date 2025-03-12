c76caff81953047d05c83e8c290950feb7fa1340
rohan pandey <rohanpandey846@gmail.com>
2025-03-12 11:42:05 +0530

    added fetch function to fetch the data from API and showing it to the table

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,52 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todos, setTodos] = useState([]);
+
+  const URL = "https://jsonplaceholder.typicode.com/todos";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try{
+        const response = await fetch(URL);
+        if(!response.ok){
+          throw new Error("Failed to fecth data!");
+        }
+        const data = await response.json();
+        setTodos(data.slice(0, 10));
+      } catch(error){
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(todos);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <table>
+          <thead>
+            <tr>
+              <th>UserId</th>
+              <th>Id</th>
+              <th>Title</th>
+            </tr>
+          </thead>
+          <tbody>
+            {todos.map((todo) => (
+              <tr key={todo.id}>
+                <td>{todo.userId}</td>
+                <td>{todo.id}</td>
+                <td>{todo.title}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
     </>
   )
 }
